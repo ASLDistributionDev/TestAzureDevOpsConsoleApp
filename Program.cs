31ecb2ecc1124c3226c083fdc847d91c1396d803
Asif Alli <alli.asif@gmail.com>
2024-02-08 17:41:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAzureDevOpsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TestAzureDevOpsConsoleApp is running...");
+            string path = ".";
+            int fCount = Directory.GetFiles(path, "*", SearchOption.AllDirectories).Length;
+            
+            for(; ; )
+            {
+                fCount = Directory.GetFiles(path, "*", SearchOption.AllDirectories).Length;
+                Console.WriteLine("# of files here: " + fCount);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
